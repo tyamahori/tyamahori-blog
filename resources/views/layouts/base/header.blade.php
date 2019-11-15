081c4ac117b8e3ae95527dfcf2193e7a18227376
tyamahori <tyamahori@gmail.com>
2019-11-15 09:11:02 +0900

    ロゴリンクの修正

--- resources/views/layouts/base/header.blade.php
+++ resources/views/layouts/base/header.blade.php
@@ -4,7 +4,7 @@
       <div class="section">
         <div class="has-text-centered">
           <h1 class="title is-1 is-spaced">
-            <a href="{{ Config::get('url') }}">
+            <a href="{{ Config::get('app.url') }}">
               <img class="logo" src="{{ asset('img/logo.png') }}" alt="{{ Config::get('app.name') }}">
             </a>
           </h1>
