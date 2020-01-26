1f21433f9a2702f4e93d04b11ffad2a57e6b9540
tyamahori <tyamahori@gmail.com>
2020-01-26 13:08:13 +0900

    dateをある程度ランダム、かつユニークにするように対応

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,23 +2,30 @@
 
 use Faker\Generator as Faker;
 use App\Eloquent\PostOrm;
+use Carbon\Carbon;
 
 $factory->define(PostOrm::class, static function (Faker $faker) {
+    $time = Carbon::now()->subMinutes($faker->unique()->numberBetween(1, 230));
     return [
         'title'       => $faker->text($maxNbChars = 20),
         'description' => $faker->text($maxNbChars = 50),
         'post'        => $faker->text($maxNbChars = 1000),
         'published'   => $faker->boolean(0),
-        'category_id' => $faker->numberBetween($min = 1, $max = 10)
+        'category_id' => $faker->numberBetween($min = 1, $max = 10),
+        'created_at'  => $time,
+        'updated_at'  => $time,
     ];
 }, 'privateMockPostData');
 
 $factory->define(PostOrm::class, static function (Faker $faker) {
+    $time = Carbon::now()->subMinutes($faker->unique()->numberBetween(1, 230));
     return [
         'title'       => $faker->text($maxNbChars = 20),
         'description' => $faker->text($maxNbChars = 50),
         'post'        => $faker->text($maxNbChars = 1000),
         'published'   => $faker->boolean(100),
-        'category_id' => $faker->numberBetween($min = 1, $max = 10)
+        'category_id' => $faker->numberBetween($min = 1, $max = 10),
+        'created_at'  => $time,
+        'updated_at'  => $time,
     ];
 }, 'publicMockPostData');
